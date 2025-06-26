214b089998000cd80690b6f04123b8c8b2882cc1
Ivan Lorenzo <elhibar77@hotmail.es>
2025-06-26 21:15:02 +0200

    Menú hamburguesa implementado en subcategory con la lógica

--- src/filtro.js
+++ src/filtro.js
@@ -4,6 +4,92 @@ const API_URL2 = "http://localhost:3000/categories";
 const productsContainer = document.getElementById("product-cards-container");
 const title = document.getElementById("name");
 
+const menuToggle = document.getElementById('menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+const btnClose = document.getElementById('close-menu');
+const tabButtons = document.querySelectorAll('.gender-tabs .tab');
+const categories = document.querySelectorAll('.categories');;
+
+menuToggle.addEventListener("click", toggleMenu);
+btnClose.addEventListener("click", closeMenu);
+document.addEventListener("click", handleClickOutside);
+
+//Funciones menú
+function toggleMenu() {
+    if (sideMenu.classList.contains("visible")) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+}
+function openMenu() {
+    sideMenu.classList.add("visible");
+    sideMenu.setAttribute("aria-hidden", "false");
+    sideMenu.classList.replace("fa-bars", "fa-xmark");
+    menuToggle.setAttribute("aria-label", "cerrar menú");
+    loadSubcategoriesMenu();
+}
+function closeMenu() {
+    sideMenu.classList.remove('visible');
+    sideMenu.setAttribute("aria-hidden", "true");
+    sideMenu.classList.replace("fa-xmark", "fa-bars");
+    menuToggle.setAttribute("aria-label", "abrir menú");
+}
+function handleClickOutside(e) {
+    const clickInsideMenu = sideMenu.contains(e.target);
+    const clickToggle = menuToggle.contains(e.target);
+    if (!clickInsideMenu && !clickToggle && sideMenu.classList.contains("visible")) {
+        closeMenu();
+    }
+}
+tabButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const targetId = button.getAttribute("data-target");
+        tabButtons.forEach((btn) => btn.classList.remove("active"));
+        button.classList.add("active");
+        categories.forEach((cat) => {
+            if (cat.id === targetId) {
+                cat.classList.remove("hidden");
+                cat.classList.add("active");
+            } else {
+                cat.classList.add("hidden");
+                cat.classList.remove("active");
+            }
+        });
+    });
+});
+
+//GET subcategorías del menú
+async function loadSubcategoriesMenu() {
+    try {
+        const res = await fetch(API_URL2);
+        const categories = await res.json();
+        console.log(categories);
+        const womanList = document.querySelector("#mujer ul");
+        const manList = document.querySelector("#hombre ul");
+        womanList.innerHTML = "";
+        manList.innerHTML = "";
+        categories.forEach((category) => {
+            category.subcategories.forEach((subcat) => {
+                const li = document.createElement("li");
+                const link = document.createElement("a");
+                link.href = `/subcategories/${subcat.id}`;
+                link.textContent = subcat.name;
+                li.appendChild(link);
+                if (category.name.toLowerCase() === "mujer") {
+                    womanList.appendChild(li);
+                } else if (category.name.toLowerCase() === "hombre") {
+                    manList.appendChild(li);
+                }
+            });
+        });
+    } catch (error) {
+        console.error(error);
+        alert("No se pudieron cargar las subcategorías ☹️");
+    }
+}
+loadSubcategoriesMenu();
+
 // Lee los parámetros de la URL
 const params = new URLSearchParams(window.location.search);
 const gender = params.get("gender") || params.get("category"); // 'mujer' o 'hombre'
